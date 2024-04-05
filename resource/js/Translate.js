73eca66e61f2634b7ffc8b5ee4c01359f4098e28
AngelGab006 <angelbeltrea.20@gmail.com>
2024-04-05 15:09:39 -0400

    add working matrix form in Translate.html

--- resource/js/Translate.js
+++ resource/js/Translate.js
@@ -15,7 +15,27 @@ function orderMatrix(arr, order) {
         orderIteration = order + iterationQuantity;
     }
     return output
-}
+};
+function outputMatrix(output) {
+    divOutput.innerHTML = `
+    <h3>Output Matrix</h3>
+    <p>${JSON.stringify(output)}</p>
+    <h3>translation</h3>
+    <p>in ascii code: <a href='https://theasciicode.com.ar/'>theasciicode</a></p>
+    `
+};
+function matrixForm(arr, order) {
+    let iterations = arr.length / order;
+    divOutput.innerHTML += '<table><caption>Matrix Form</caption><tbody id="matrixForm"></tbody></table>'
+    let Table = document.getElementById('matrixForm')
+    for (let i = 0; i < iterations; i++) {
+        Table.innerHTML += '<tr></tr>'
+        let tr = Table.lastChild
+            for (let ind = 0; ind < order; ind++) {
+                tr.innerHTML += `<td>${arr[i][ind]}</td>`
+            }
+    }
+};
 
 form.addEventListener('submit', e=> {
     const textarea = form.elements['text-to-translate'];
@@ -27,12 +47,8 @@ form.addEventListener('submit', e=> {
         let transformTextArr = textArr.map(e=> e.charCodeAt(0))
         //to matrix order
         let output = orderMatrix(transformTextArr,order)
-        divOutput.innerHTML = `
-        <h3>Output Matrix</h3>
-        <p>${JSON.stringify(output)}</p>
-        <h3>translation</h3>
-        <p>in ascii code: <a href='https://theasciicode.com.ar/'>theasciicode</a></p>
-        `
+        outputMatrix(output);
+        matrixForm(output,order)
     }
-    event.preventDefault();
+    e.preventDefault();
 })
