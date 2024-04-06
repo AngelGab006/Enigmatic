0bb698c35d855e2f6ad0e32536d33f865945c35b
AngelGab006 <angelbeltrea.20@gmail.com>
2024-04-06 17:15:09 -0400

    fix error with fractions in decrypt

--- resource/js/decrypt.js
+++ resource/js/decrypt.js
@@ -8,6 +8,17 @@ const MatrixProd = (A, B) =>
       )
     )
 );
+function Matrix(A,B){
+    let output = MatrixProd(A,B).map(e=>{
+      let out=[]
+      e.forEach(el=>{
+        out.push(Math.floor(el+0.1))
+      })
+      return out
+    })
+    return output
+  }
+  
 const IsAPossibleProduct = (A,B) => {
     return(A[A.length-1].length === B.length)
 }
@@ -16,15 +27,16 @@ function Table(params) {
     
 }
 form.addEventListener('submit', e=> {
-    const arrA = JSON.parse(form.elements['arrA'].value);
-    const arrB = JSON.parse(form.elements['arrB'].value);
+    e.preventDefault();
+    const arrA = eval(form.elements['arrA'].value);
+    const arrB = eval(form.elements['arrB'].value);
     const type = form.elements['Type'];
     const order = Number(form.elements['order'].value);
     if (type.value === '1') {
         let transformArrText;
         if(IsAPossibleProduct(arrA, arrB)){
             //get matrix 
-            let matrix = MatrixProd(arrA,arrB);
+            let matrix = Matrix(arrA,arrB);
             //transform to string
             transformArrText = matrix.map(e=>{
                 let output = '';
@@ -33,8 +45,9 @@ form.addEventListener('submit', e=> {
                 });
                 return output
             }); 
+        }else{
+            transformArrText = [['is'],[' '],['not'],[' '],['a'],[' '],['possible'],[' '],['product']]
         }
         Output(transformArrText.join(''));
     }
-    e.preventDefault();
 })
